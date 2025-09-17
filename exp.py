8e370da6aea8ec4c7d1d2b4fbba9b1114bd8ab21
Alex <kennyobla123@gmail.com>
2025-09-17 12:54:00 +0300

    updated physics

--- exp.py
+++ exp.py
@@ -5,7 +5,7 @@ from vpython import scene
 import model
 import random
 
-vp.scene.title = "Modeling the motion of planets with the gravitational force"
+vp.scene.title = ""
 vp.scene.height = 800
 vp.scene.width = 1200
 
@@ -18,85 +18,13 @@ def gravitationalForce(p1, p2):
     F = - rHat * G * p1.mass * p2.mass / rMagnitude ** 2
     return F
 
-
-# Класс для управления частицами выхлопа
-class RocketExhaust:
-    def __init__(self, rocket):
-        self.rocket = rocket
-        self.particles = []
-        self.max_particles = 300
-        self.particle_lifetime = 0.5  # время жизни частицы в секундах
-
-    def create_particle(self):
-        # Создаем частицу у основания ракеты
-        rocket_bottom = self.rocket.pos - vp.vector(0, self.rocket.size.y / 2, 0)
-
-        # Случайное смещение у основания
-        offset_x = random.uniform(-self.rocket.size.x / 3, self.rocket.size.x / 3)
-        offset_z = random.uniform(-self.rocket.size.z / 3, self.rocket.size.z / 3)
-
-        # СИНЕВАТЫЕ ЦВЕТА для частиц
-        blue_colors = [
-            vp.vector(0.0, 0.5, 1.0),  # Ярко-синий
-            vp.vector(0.2, 0.6, 1.0),  # Светло-синий
-            vp.vector(0.0, 0.4, 0.8),  # Темно-синий
-            vp.vector(0.3, 0.7, 1.0),  # Голубоватый
-            vp.vector(0.1, 0.3, 0.9)  # Фиолетово-синий
-        ]
-
-        particle = vp.sphere(
-            pos=rocket_bottom + vp.vector(offset_x, 0, offset_z),
-            radius=random.uniform(20,20),
-            color=random.choice(blue_colors),
-            opacity=random.uniform(0.6, 0.8),
-            make_trail=False
-        )
-
-        # Начальная скорость частицы (вниз + случайное рассеивание)
-        velocity = vp.vector(
-            random.uniform(-0.2, 0.2),
-            random.uniform(-2.0, -0.5),
-            random.uniform(-0.2, 0.2)
-        )
-
-        return {'obj': particle, 'velocity': velocity, 'lifetime': 0}
-
-    def update(self, dt):
-        # Удаляем старые частицы
-        for i in range(len(self.particles) - 1, -1, -1):
-            self.particles[i]['lifetime'] += dt
-            if self.particles[i]['lifetime'] > self.particle_lifetime:
-                self.particles[i]['obj'].visible = False
-                del self.particles[i]
-
-        # Добавляем новые частицы
-        if len(self.particles) < self.max_particles:
-            for _ in range(3):  # создаем несколько частиц за раз
-                if len(self.particles) < self.max_particles:
-                    self.particles.append(self.create_particle())
-
-        # Обновляем существующие частицы
-        for particle in self.particles:
-            # Двигаем частицу
-            particle['obj'].pos += particle['velocity'] * dt
-
-            # Уменьшаем размер и прозрачность со временем
-            age_ratio = particle['lifetime'] / self.particle_lifetime
-            particle['obj'].radius *= 0.95
-            particle['obj'].opacity *= 0.9
-
-            # Добавляем случайное движение для эффекта турбулентности
-            particle['velocity'] += vp.vector(
-                random.uniform(-0.1, 0.1),
-                random.uniform(-0.05, 0.05),
-                random.uniform(-0.1, 0.1)
-            ) * dt
-
-
 t = 0
 dt = model.DELTA_TIME
 earth = vp.sphere(pos=vp.vector(0, 0, 0), radius=model.RADIUS_OF_EARTH, color=vp.color.green,
-                  mass=1000, momentum=vp.vector(0, 0, 0))
+                  momentum=vp.vector(0, 0, 0))
+
+orbit = vp.sphere(pos=vp.vector(0, 0, 0), radius=model.RADIUS_OF_EARTH + model.ISS_ALTITUDE, color=vp.color.blue, opacity = 0.2,
+                  momentum=vp.vector(0, 0, 0))
 
 rocket = vp.box(
     pos=vp.vector(0, model.RADIUS_OF_EARTH, 0),
@@ -105,11 +33,21 @@ rocket = vp.box(
     radius=0.0001,
     color=vp.color.red,
     make_trail=True,
-    trail_color=vp.color.cyan
+    trail_color=vp.color.cyan,
 )
 
-# Создаем систему выхлопа
-rocket_exhaust = RocketExhaust(rocket)
+arrow = vp.arrow(
+    color = vp.color.red,
+    size=vp.vector(2*model.RADIUS_OF_EARTH, 0, 0), shaftwidth = 10)
+
+arrow2 = vp.arrow(
+    color = vp.color.red,
+    size=vp.vector(2*model.RADIUS_OF_EARTH, 0, 0), shaftwidth = 30)
+
+arrow3 = vp.arrow(
+    color = vp.color.red,
+    size=vp.vector(2*model.RADIUS_OF_EARTH, 0, 0), shaftwidth = 30)
+
 
 # Добавляем освещение
 vp.distant_light(direction=vp.vector(1, 1, 1), color=vp.color.white)
@@ -153,8 +91,6 @@ while True:
     # Обновляем время
     t += dt
 
-    # Обновляем систему выхлопа
-    rocket_exhaust.update(dt)
 
     # Обновляем позицию МКС
     angle = angular_velocity * t
@@ -173,3 +109,17 @@ while True:
         rocket.up = vp.vector(world_model.rocket.velocity.GetX(),
                               world_model.rocket.velocity.GetY(),
                               world_model.rocket.velocity.GetZ())
+    arrow.axis = vp.vector(world_model.rocket.reactive_force.GetX() / 10000, world_model.rocket.reactive_force.GetY() / 10000, world_model.rocket.reactive_force.GetZ())
+    arrow.pos = vp.vector(world_model.rocket.center_radius.GetX(), world_model.rocket.center_radius.GetY(), world_model.rocket.center_radius.GetZ())
+
+    vp.scene.title = str("Реактивная сила: " + str(int(world_model.rocket.reactive_force.GetModulus())) + " H. Скорость ракеты: " + str(int(world_model.rocket.velocity.GetModulus())) + " м/c." + " Высота над поверхностью земли: " + str(int(world_model.rocket.center_radius.GetModulus() - model.RADIUS_OF_EARTH)) + " м")
+
+    """arrow2.axis = vp.vector(world_model.rocket.future_position_delta.GetX(), world_model.rocket.future_position_delta.GetY(), world_model.rocket.future_position_delta.GetZ())
+    arrow2.pos = vp.vector(world_model.rocket.future_position_center.GetX(), world_model.rocket.future_position_center.GetY(),
+                          world_model.rocket.future_position_center.GetZ())
+
+    arrow3.axis = vp.vector(world_model.rocket.delta.GetX(),
+                            world_model.rocket.delta.GetY(),
+                            world_model.rocket.delta.GetZ())
+    arrow3.pos = vp.vector(world_model.rocket.future_position_center.GetX(), world_model.rocket.future_position_center.GetY(),
+                          world_model.rocket.future_position_center.GetZ())"""
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from random import randint
 
 MASS_OF_EARTH = 5.97e24
 RADIUS_OF_EARTH = 6_378_000
-ISS_ALTITUDE = 408
+ISS_ALTITUDE = 10_000
 ISS_PERIOD = 92.5  # минут (период обращения МКС)
 SIMULATION_SPEED = 50
 DELTA_TIME = 0.01
@@ -43,9 +43,9 @@ class Vector:
 
     def SetModulus(self, modulus):
         old_modulus = self.GetModulus()
-        self.x_cord *= modulus / old_modulus
-        self.y_cord *= modulus / old_modulus
-        self.z_cord *= modulus / old_modulus
+        self.x_cord *= (modulus / old_modulus)
+        self.y_cord *= (modulus / old_modulus)
+        self.z_cord *= (modulus / old_modulus)
 
     def CopyVector(self):
         return Vector(self.x_cord, self.y_cord, self.z_cord)
@@ -89,8 +89,8 @@ class Rocket:
     def __init__(self):
         # basic characteristics
         self.mass = 300_000
-        self.mass_fuel_consumption = 88.3
-        self.gas_speed_relative_to_the_rocket = 400000
+        self.mass_fuel_consumption = 300
+        self.gas_speed_relative_to_the_rocket = 16000
 
         # dynamics
         self.reactive_force = Vector(0, self.mass_fuel_consumption * self.gas_speed_relative_to_the_rocket, 0)
@@ -101,61 +101,103 @@ class Rocket:
 
         self.center_radius = Vector(0, RADIUS_OF_EARTH, 0)
 
-        self.target = Vector(0, RADIUS_OF_EARTH + 10000, 0)
+        self.target = Vector(0, RADIUS_OF_EARTH + ISS_ALTITUDE, 0)
         self.orbital_altitude = self.target.GetModulus() - RADIUS_OF_EARTH
 
         self.reactive_force_angle = 0
         self.delta_target_altitude = abs(self.target.GetModulus() - self.center_radius.GetModulus())
+        self.future_position_center = self.center_radius.CopyVector()
+        self.future_position_delta = Vector(1000, 0, 0)
+        self.future_position = Vector(1000, 0, 0)
+        self.delta = Vector(1000, 0, 0)
+        self.velocity_per = Vector(1000, 0, 0)
 
         x_angle = math.pi / 2
         y_angle = math.pi / 2
         z_angle = 0
 
-    def ComputeTangentialForce(self, external_force):
-        vector_a = self.center_radius.CopyVector()
-        vector_a.AddVector(self.target * -1)
-        vector_b = self.target * -1
-        vector_a_minus_b = self.center_radius.CopyVector()
-        vector_c = vector_a * ((vector_b.ScalarProduct(vector_b) - vector_a.ScalarProduct(vector_b)) / (vector_a_minus_b.GetModulus() ** 2))
-        vector_c.AddVector(vector_b * ((vector_a.ScalarProduct(vector_a) - vector_a.ScalarProduct(vector_b)) / (vector_a_minus_b.GetModulus() ** 2)))
-        self.reactive_force.SetDirection(vector_c * -1)
-        self.reactive_force.AddVector(external_force * -1)
+    def ComputeNormalForce(self, external_force):
+        if self.center_radius.ScalarProduct(self.velocity) > 50:
+            self.reactive_force.SetDirection(Vector(self.velocity.GetY(), -self.velocity.GetX(), self.velocity.GetZ()))
+            self.reactive_force.SetModulus(self.mass_fuel_consumption * self.gas_speed_relative_to_the_rocket)
+        elif self.center_radius.ScalarProduct(self.velocity) < -50:
+            self.reactive_force.SetDirection(Vector(-self.velocity.GetY(), self.velocity.GetX(), self.velocity.GetZ()))
+            self.reactive_force.SetModulus(self.mass_fuel_consumption * self.gas_speed_relative_to_the_rocket)
+        else:
+            self.reactive_force.SetDirection(external_force)
+            self.reactive_force.SetModulus(self.mass * (self.velocity.GetModulus() ** 2) / self.center_radius.GetModulus() - external_force.GetModulus())
 
 
     def ComputeReactiveForce(self, external_force):
         if self.mass < self.MIN_ROCKET_WEIGHT:
             self.reactive_force = Vector()
         else:
-            if abs(self.center_radius.GetModulus() - self.target.GetModulus()) < 10:
-                self.ComputeTangentialForce(external_force)
+            if abs(self.center_radius.GetModulus() - self.target.GetModulus()) < 30:
+                self.ComputeNormalForce(external_force)
             else:
-                if self.velocity.GetModulus() < 300:
+                print("velocity = ", self.velocity.GetModulus())
+                if self.velocity.GetModulus() < 30:
                     return
 
+                self.delta_target_altitude = self.target.GetModulus() - self.center_radius.GetModulus()
                 print(self.delta_target_altitude)
-
                 center_radius_of_trajectory = self.center_radius.CopyVector()
-                delta_radius = Vector(-self.velocity.GetY(), self.velocity.GetX(), self.velocity.GetZ())
+                delta_radius = Vector(self.velocity.GetY(), -self.velocity.GetX(), self.velocity.GetZ())
+                self.velocity_per = delta_radius.CopyVector()
+                #self.delta = delta_radius.CopyVector()
+                #self.delta *= 10
+                suppose_angle_left = math.acos(
+                    self.velocity.ScalarProduct(Vector(0, 1, 0)) / self.velocity.GetModulus())
+                suppose_angle_right = math.acos(
+                    self.velocity.ScalarProduct(Vector(0, 1, 0)) / self.velocity.GetModulus()) + math.pi / 2
 
-                suppose_angle = random.uniform(self.reactive_force_angle - math.pi / 6, self.reactive_force_angle + math.pi / 6)
-                suppose_force = self.reactive_force.CopyVector()
-                suppose_force.SetDirection(Vector(math.sin(suppose_angle), math.cos(suppose_angle), 0))
-                suppose_reactive_force = suppose_force.CopyVector()
-                suppose_force.AddVector(external_force)
-                suppose_force_projection = abs(suppose_force.ScalarProduct(delta_radius)) / delta_radius.GetModulus()
-                suppose_acceleration = suppose_force_projection / self.mass
+                suppose_reactive_force = Vector()
+                while suppose_angle_right - suppose_angle_left > 0.0001:
+                    suppose_angle = (suppose_angle_right + suppose_angle_left) / 2
+                    suppose_force = self.reactive_force.CopyVector()
+                    suppose_force.SetDirection(Vector(math.sin(suppose_angle), math.cos(suppose_angle), 0))
+                    suppose_reactive_force = suppose_force.CopyVector()
+                    suppose_force.AddVector(external_force)
+                    suppose_force_projection = abs(suppose_force.ScalarProduct(delta_radius)) / delta_radius.GetModulus()
+                    suppose_acceleration = suppose_force_projection / self.mass
+                    if suppose_acceleration == 0:
+                        return
+                    delta_radius.SetModulus((self.velocity.GetModulus() ** 2) / suppose_acceleration)
+                    delta_radius.SetDirection(Vector(self.velocity.GetY(), -self.velocity.GetX(), self.velocity.GetZ()))
+                    center_radius_of_trajectory.AddVector(delta_radius)
+                    self.delta = delta_radius.CopyVector()
+                    delta_radius.SetDirection(center_radius_of_trajectory)
+                    suppose_position = center_radius_of_trajectory.CopyVector()
+                    suppose_position.AddVector(delta_radius)
+                    self.future_position_center = center_radius_of_trajectory.CopyVector()
+                    self.future_position_delta = delta_radius.CopyVector()
+                    delta_altitude = suppose_position.GetModulus() - self.target.GetModulus()
+                    if delta_altitude > 0:
+                        suppose_angle_left = suppose_angle
+                    else:
+                        suppose_angle_right = suppose_angle
+                self.reactive_force = suppose_reactive_force.CopyVector()
+                #print("delta = ", self.delta.GetX())
 
-                delta_radius.SetModulus((self.velocity.GetModulus() ** 2) / suppose_acceleration)
-                center_radius_of_trajectory.AddVector(delta_radius)
-                delta_radius.SetDirection(center_radius_of_trajectory)
-                suppose_position = center_radius_of_trajectory.CopyVector()
-                suppose_position.AddVector(delta_radius)
-                delta_altitude = abs(suppose_position.GetModulus() - self.target.GetModulus())
+                #suppose_angle = random.uniform(self.reactive_force_angle - math.pi / 6, self.reactive_force_angle + math.pi / 6)
+                #suppose_force = self.reactive_force.CopyVector()
+                #suppose_force.SetDirection(Vector(math.sin(suppose_angle), math.cos(suppose_angle), 0))
+                #suppose_reactive_force = suppose_force.CopyVector()
+                #suppose_force.AddVector(external_force)
+                #suppose_force_projection = abs(suppose_force.ScalarProduct(delta_radius)) / delta_radius.GetModulus()
+                #suppose_acceleration = suppose_force_projection / self.mass
 
-                if delta_altitude < self.delta_target_altitude:
-                    self.reactive_force = suppose_reactive_force.CopyVector()
-                    self.delta_target_altitude = delta_altitude
-                    self.reactive_force_angle = suppose_angle
+                #delta_radius.SetModulus((self.velocity.GetModulus() ** 2) / suppose_acceleration)
+                #center_radius_of_trajectory.AddVector(delta_radius)
+                #delta_radius.SetDirection(center_radius_of_trajectory)
+                #suppose_position = center_radius_of_trajectory.CopyVector()
+                #suppose_position.AddVector(delta_radius)
+                #delta_altitude = abs(suppose_position.GetModulus() - self.target.GetModulus())
+
+                #if delta_altitude < self.delta_target_altitude:
+                #    self.reactive_force = suppose_reactive_force.CopyVector()
+                #    self.delta_target_altitude = delta_altitude
+                #    self.reactive_force_angle = suppose_angle
                 """else:
                     p = math.exp((self.delta_target_altitude - delta_altitude) / abs(self.target.GetModulus() - self.center_radius.GetModulus()))
                     N = 1_000_000
